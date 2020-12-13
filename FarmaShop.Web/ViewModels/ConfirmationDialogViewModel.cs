bda03801e9e4bed885cf8fbb4e9c1f98f47aaaf2
Catalin Mihai <catalinnm99@gmail.com>
2020-12-13 22:16:15 +0200

    Add a Partial View Component that will dynamically show a custom confirmation dialog. Very useful for deletions.

---
+++ FarmaShop.Web/ViewModels/ConfirmationDialogViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace FarmaShop.Web.ViewModels
+{
+    public class ConfirmationDialogViewModel
+    {
+        public string DialogMessage { set; get; }
+        public string DismissButton { set; get; }
+        public string ConfirmButton { set; get; }
+        
+        public string AjaxMethod { set; get; }
+        public string AjaxUrl { set; get; }
+
+        public string RedirectLocation { set; get; }
+        
+        public string[] Parameters { get; set; }
+        
+        public string SuccesMessage { set; get; }
+        
+        public string FailMessage { set; get; }
+    }
+}
